35f169280f9270f5f1069a36a444117b1a9addce
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2025-05-27 17:02:30 +0700

    commit 6

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -3,3 +3,17 @@
 using namespace std;
 
 class dokter;
+class paien; { // asosiasi
+    public:
+        string nama;
+        vector<dokter*> daftar_dokter;
+        pasien(string pNama): nama(pNama) {
+            cout << "Pasien \"" << nama << "\" ada\n";
+        }
+        ~pasien() {
+            cout << "Pasien \"" << nama << "\" tidak ada\n";
+        }
+        void tambahDokter(dokter*);
+        void cetakDokter();
+};
+
